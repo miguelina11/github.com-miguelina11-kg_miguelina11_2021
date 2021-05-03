3b32e55dc7d5d4013ed38010ba053b478f2cdd38
miguelina11 <miguelinaespinal2020@github.com>
2021-05-03 13:37:47 -0400

    Update Arrays.js
    Loop Counter was added in line 4 to make sure all arrays are counted and general spelling errors and extra parentheses errors were fixed

--- Arrays.js
+++ Arrays.js
@@ -1,15 +1,14 @@
-var argument=process.argc.slice(2);
+var argument=process.argv.slice(2);
 var numbers=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
 var inTexts=[];
-my argument.foreach(function(number)){
+var i = 0;
+my argument.forEach(function(number)){
 value="";
 invalues=numbers.split("");
 invalues.forEach(function(digit){
-value=numbers[parsenlnt(digit)]{
   value +=numbers[parsenlnt(digit);
-}}
+}
 intexts[i] = value;
 i++;
 }
 console.log(intexts.join(","))
-}
