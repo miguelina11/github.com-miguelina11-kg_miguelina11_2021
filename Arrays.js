14c55de1b47f55df5abeb4d8e338bc360afce5e8
miguelina11 <miguelinaespinal2020@github.com>
2021-05-03 13:14:29 -0400

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,11 @@
+var argument=process.argc.slice(2);
+var numbers=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"];
+var inTexts=[];
+my argument.foreach(function(number)){
+value="";
+invalues=numbers.split("");
+invalues.forEach(function(digit){
+value=numbers[parsenlnt(digi)];
+}}
+
+}
